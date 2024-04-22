75d8e184f3ffa77d96dab23e2dce7a143b62c424
Hazimi234 <meorhazimi05@gmail.com>
2024-04-22 14:11:26 +0800

    changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__,template_folder='templates')
 
@@ -6,6 +6,17 @@ app = Flask(__name__,template_folder='templates')
 def home():
     return render_template("home.html")
 
+@app.route("/user-login", methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("user_login.html")
+    
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
