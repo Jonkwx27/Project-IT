4db7a806f59e4eaf1f566f95c5c16478e494b653
Jonkwx27 <1221109226@soffice.mmu.edu.my>
2024-04-22 12:15:14 +0800

    changes to main.py
    - added route
    - able to run the url link and connect to html

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from web import create_app
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    return app
 
 app = create_app()
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/signup')
+def signup():
+    return 'Sign Up'
+
+@app.route('/login')
+def login():
+    return 'Log In'
 
 if __name__ == '__main__':
     app.run(debug=True)
